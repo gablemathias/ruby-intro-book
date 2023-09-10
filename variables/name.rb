35dc79f84c5962ab16d6a2bef6fa1b0fb40e4556
Gabriel Mathias <gablemathias@icloud.com>
2023-09-10 07:48:32 -0300

    ex 4 and 5

--- variables/name.rb
+++ variables/name.rb
@@ -1,4 +1,8 @@
-puts "Hello, what's your name?"
-name = gets.chomp
+puts "Hello, what's your first name?"
+first_name = gets.chomp
+puts "And last name?"
+last_name  = gets.chomp
 
-puts "Welcome, #{name}."
+10.times do |i|
+  puts "#{i+1}. Welcome, #{first_name} #{last_name}."
+end
